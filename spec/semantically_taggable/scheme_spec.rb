c8cecaebe20c705e2c3799bda168157bed3a9409
Russell Garner <rgarner@zephyros-systems.co.uk>
2011-01-12 18:39:05 +0000

    Version bump to 0.1.1

---
+++ spec/semantically_taggable/scheme_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe "Importing from SKOS" do
+
+  it "should do something" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
